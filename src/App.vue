ba2df02bcb6c7f33926d4dccbc54fcab43213fd4
themattfromamgers <133354306+themattfromamgers@users.noreply.github.com>
2023-05-12 13:12:07 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from './components/Header.vue';
+
+</script>
+
+<template>
+  <div>
+  </div>
+  <Header text="Header Text" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from 'vue'
+defineProps({
+    text: String
+})
+
+const todoName = ref('')
+
+let todoList = ref([])
+const addTodo = ()=>{
+    todoList.value.push({
+        id: todoList.value.length + 1,
+        text: todoName.value
+    })
+    console.log(todoList)
+}
+
+const deleteTodo = (todoItem) => {
+  const index = todoList.value.findIndex((todo) => todo.id === todoItem.id);
+  if (index !== -1) {
+    todoList.value.splice(index, 1);
+  }
+};
+
+</script>
+
+<template>
+    <h1>TODO APP</h1>
+    {{ todoName }}
+    <input type="text" v-model="todoName">
+    <button type="submit" @click="addTodo">Ekle</button>
+
+    <ul>
+       <li v-for="i in todoList" :key={i}>{{ i.text }} <button @click="deleteTodo(i)">x</button></li> 
+    </ul>
+</template>
+
+<style scoped>
+</style>
